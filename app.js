1a748435a6e86dd1191951f0bc83fb605bc77108
Opilane TTHK <opilane@tthk.ee>
2020-01-07 15:46:09 +0200

    ayo

---
+++ app.js
@@ -0,0 +1,23 @@
+document.querySelector('.get-jokes').addEventListener("click", getJokes);
+
+function getJokes(event) {
+    const number = document.querySelector('input[type="number"]').value;
+    console.log(number);
+
+    fetch(`http://api.icndb.com/jokes/random/${number}`)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        let output = "";
+        console.log(data);
+
+       data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+        });
+
+        document.querySelector('.jokes').innerHTML = output;
+    })
+    event.preventDefault();
+}
